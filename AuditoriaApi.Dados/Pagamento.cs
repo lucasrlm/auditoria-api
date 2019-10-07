85e08f66124b1f0b9da745289217860f056f11b6
Lucas Raphael Leão Martins <lucas.raphael@dtidigital.com.br>
2019-10-06 21:16:55 -0300

    POC de api de auditoria implementada

---
+++ AuditoriaApi.Dados/Pagamento.cs
@@ -0,0 +1,12 @@
+﻿namespace AuditoriaApi.Dados
+{
+    public class Pagamento
+    {
+        public decimal Valor { get; set; }
+
+        public string RegistrarPagamento()
+        {
+            return $@"Foi realizado um pagamento no valor de: R$ {Valor.ToString("0.##")}.";
+        }
+    }
+}
---
+++ AuditoriaApi/Controllers/PedidosController.cs
@@ -0,0 +1,34 @@
+﻿using AuditoriaApi.Dados;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace auditoria_api.Controllers
+{
+    /// <summary>
+    /// Controller de auditoria de pedidos
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidosController : ControllerBase
+    {
+
+        ILogger _logger;
+        public PedidosController(ILogger<PedidosController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Registra os dados de um pagamento que foi realizado
+        /// </summary>
+        /// <param name="pagamento"></param>
+        /// <returns></returns>
+        [HttpPost("pagamento")]
+        public ActionResult RegistrarPagamento([FromBody]Pagamento pagamento)
+        {
+            string log = pagamento.RegistrarPagamento();
+            _logger.LogInformation(log);
+            return Ok(log);
+        }   
+    }
+}
